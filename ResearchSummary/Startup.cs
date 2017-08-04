659e214948a22070bf540681f8295edc3885b4a0
yrui <yrui@hawaii.edu>
2017-08-04 10:05:48 -1000

    InitialProject

---
+++ ResearchSummary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResearchSummary.Startup))]
+namespace ResearchSummary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
